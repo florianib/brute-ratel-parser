29b559b442f1ae3c7384a067346de7d4c1daa70a
floriani <benjamin.floriani@k-business.com>
2024-08-22 10:31:51 +0200

    changed flags

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,15 @@
 use brute_ratel_parser::{combine, create_excel};
-use clap::{arg, command, Parser};
+use clap::{arg, command, ArgAction, Parser};
 use std::path::PathBuf;
 
 #[derive(Parser)]
 #[command(version, about, long_about = None)]
 struct Cli {
-    #[clap(long, action)]
+    #[clap(long, action=ArgAction::SetFalse, long="no-combine")]
     combine: bool,
     #[arg(short, long)]
     path: PathBuf,
-    #[clap(long, action)]
+    #[clap(long, action=ArgAction::SetFalse, long="no-generate")]
     generate: bool,
 }
 
